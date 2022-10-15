e0119bf22de6b25ffc38cbd7a63168cf2742d004
seungmin-lee-dotcom <k07217@naver.com>
2022-10-15 16:20:43 +0900

    final correction

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,17 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "e7896b715bc9af7ff3113d8be562f2fe";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("You live in", lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoError() {
